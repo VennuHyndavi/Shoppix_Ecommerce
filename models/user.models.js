368f68b377dbc3712975a43c7e0f8e1350c81dee
VennuHyndavi <43744805+VennuHyndavi@users.noreply.github.com>
2024-05-14 11:39:41 -0400

    Add files via upload

---
+++ models/user.models.js
@@ -0,0 +1,67 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    userId: { type: Number, unique: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    confirmPassword: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+    registrationDate: {
+        type: Date,
+        default: Date.now,
+        get: function () {
+            const date = new Date(this._doc.registrationDate);
+            const optionsDate = {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+            };
+            const optionsTime = {
+                hour: '2-digit',
+                minute: '2-digit',
+                hour12: true,
+            };
+
+            const formattedDate = date.toLocaleString('en-IN', optionsDate);
+            const formattedTime = date.toLocaleString('en-IN', optionsTime);
+
+            return `${formattedDate} ${formattedTime}`;
+        },
+    },
+});
+
+userSchema.pre('validate', function (next) {
+    if (this.isModified('password') || this.isNew) {
+        if (this.password !== this.confirmPassword) {
+            const err = new Error('Password and confirm password must match.');
+            next(err);
+        } else {
+            next();
+        }
+    } else {
+        next();
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+async function getNextUserId() {
+    try {
+        const lastUser = await User.findOne({}, {}, { sort: { 'userId': -1 } });
+
+        let nextUserId;
+
+        if (lastUser && lastUser.userId) {
+            nextUserId = lastUser.userId + 1;
+        } else {
+            nextUserId = 1;
+        }
+        return nextUserId;
+    } catch (error) {
+        console.error('Error getting the next userId:', error.message);
+        throw error;
+    }
+}
+
+module.exports = { User, getNextUserId };
